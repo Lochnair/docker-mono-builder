d757c390bcb9ef35c24c991be8e17f6ae5604b8a
Nils Andreas Svee <me@lochnair.net>
2017-02-05 19:07:35 +0100

    Run most commands as builder

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -2,16 +2,18 @@
 sed -i "s/#MAINTAINER=.*/MAINTAINER=\"$MAINTAINER\"/" /etc/abuild.conf
 sed -i "s/#PACKAGER=.*/PACKAGER=\"$PACKAGER\"/" /etc/abuild.conf
 
-echo "$PRIV_KEY" > /home/builder/.abuild/privkey.rsa
-echo 'PACKAGE_PRIVKEY="/home/builder/.abuild/privkey.rsa"' > /home/builder/.abuild/abuild.conf
+su-exec builder mkdir /home/builder/.abuild
+su-exec builder echo "$PRIV_KEY" > /home/builder/.abuild/privkey.rsa
+su-exec builder echo 'PACKAGE_PRIVKEY="/home/builder/.abuild/privkey.rsa"' > /home/builder/.abuild/abuild.conf
 
 mkdir /build
+chown builder: /build
 cd /build
 
-curl -L -O "http://git.alpinelinux.org/cgit/aports/plain/testing/mono/APKBUILD"
-curl -L -O "http://git.alpinelinux.org/cgit/aports/plain/testing/mono/arm-remove-sigcontext-include.patch"
+su-exec builder curl -L -O "http://git.alpinelinux.org/cgit/aports/plain/testing/mono/APKBUILD"
+su-exec builder curl -L -O "http://git.alpinelinux.org/cgit/aports/plain/testing/mono/arm-remove-sigcontext-include.patch"
 
-sed -i 's/pkgver=.*/pkgver=4.6.2.16/' APKBUILD
-sed -i 's/pkgrel=0/pkgrel=1/'
+su-exec builder sed -i 's/pkgver=.*/pkgver=4.6.2.16/' APKBUILD
+su-exec builder sed -i 's/pkgrel=0/pkgrel=1/'
 
-abuild rootpkg
+su-exec builder abuild rootpkg
