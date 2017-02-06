31a9ced8514b081514cccd951295f92483b7da24
Nils Andreas Svee <me@lochnair.net>
2017-02-06 19:17:52 +0100

    Add APKBUILD to project. Also try enabling boehm and sigaltstack

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -5,15 +5,8 @@ sed -i "s/#PACKAGER=.*/PACKAGER=\"$PACKAGER\"/" /etc/abuild.conf
 chown -R builder: "/home/builder/"
 su-exec builder echo 'PACKAGER_PRIVKEY="/home/builder/.abuild/privkey.rsa"' > /home/builder/.abuild/abuild.conf
 
-mkdir /build
 chown builder: /build
 cd /build
-
-su-exec builder curl -L -O "http://git.alpinelinux.org/cgit/aports/plain/testing/mono/APKBUILD"
-su-exec builder curl -L -O "http://git.alpinelinux.org/cgit/aports/plain/testing/mono/arm-remove-sigcontext-include.patch"
-
-su-exec builder sed -i 's/pkgver=.*/pkgver=4.6.2.16/' APKBUILD
-su-exec builder sed -i 's/pkgrel=0/pkgrel=1/' APKBUILD
 
 apk add \
     --no-cache \
